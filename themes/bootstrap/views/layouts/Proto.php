6874bdaa878deb485e369003b5a4b52b351aa9d5
Altenrion <landerfeld@gmail.com>
2014-08-14 23:35:27 +0400

    дубль 2

--- themes/bootstrap/views/layouts/Proto.php
+++ themes/bootstrap/views/layouts/Proto.php
@@ -73,7 +73,7 @@
                     <ul class="social pull-right">
 
                         <? if($this->checkRole(array('Dev','Manager','Exp','Exp1','Exp2','Exp3'))): ?>
-                            <li><a href="<?=Yii::app()->createUrl('Autorized/index')?>"><i class="icon-user"></i></a></li>
+                            <li><a href="<?=Yii::app()->createUrl('Autorized/profile')?>"><i class="icon-user"></i></a></li>
                         <? endif; ?>
                         <? if($this->checkRole(array('Dev','Manager','Exp','Exp1','Exp2','Exp3'))):  ?>
                             <li><a href="<?=Yii::app()->createUrl('ShowCase/logout')?>"><i class="icon-logout"></i></a></li>
