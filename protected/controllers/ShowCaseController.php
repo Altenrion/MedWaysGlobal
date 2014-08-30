2d65cc5230e77481bea48e083e66a466bb49bb93
Altenrion <landerfeld@gmail.com>
2014-08-30 20:55:54 +0400

    Снова комичу тоже самое блин

--- protected/controllers/ShowCaseController.php
+++ protected/controllers/ShowCaseController.php
@@ -38,7 +38,8 @@ class ShowCaseController extends Controller
     public function actionIndex()
 	{
         $projects = Users::model()->count("roles = 'Manager' AND AKTIV_KEY = 100");
-		$this->render('index',array('projects'=>$projects));
+        $clean_num = $this->getTermination($projects);
+		$this->render('index',array('clean_num'=>$clean_num));
 
 	}
 
@@ -337,5 +338,24 @@ class ShowCaseController extends Controller
         $this->redirect(Yii::app()->createUrl('ShowCase/login'));
     }
 
+    public function getTermination ($num)
+    {
+        //Оставляем две последние цифры от $num
+        $number = substr($num, -2);
 
+        if($number > 10 and $number < 21)
+        {
+            $term = "ов";
+        }
+        else
+        {
+            $number = substr($number, -1);
+
+            if($number == 0) {$term = "ов";}
+            if($number == 1 ) {$term = "";}
+            if($number > 1 ) {$term = "а";}
+            if($number > 4 ) {$term = "ов";}
+        }
+        return  $num.' проект'.$term;
+    }
 }
