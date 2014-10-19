79dd4fc3811da2838a46dd577984fa62530f7a30
Altenrion <landerfeld@gmail.com>
2014-10-19 15:11:24 +0400

    добавил редактирование платформ после сортировок

--- themes/bootstrap/views/autorized/_ExpertsList.php
+++ themes/bootstrap/views/autorized/_ExpertsList.php
@@ -16,7 +16,26 @@
         $('tbody').on('mouseover', 'tr', function(){
         <? foreach($nums as $id_k=>$id_v): ?>
             $('a[rel="roles_<?=$id_v ?>"]').editable({'disabled':false,'showbuttons':false,'name':'roles','title':'Выберите роль','value':null,'url':'<?= Yii::app()->createUrl('Autorized/updateProfile') ?>','type':'select','placement':'top','source':[{'value':'Exp','text':'Эксперт0'},{'value':'Exp1','text':'Эксперт1'},{'value':'Exp2','text':'Эксперт2'},{'value':'Exp3','text':'Эксперт3'}]});
-        <? endforeach; ?>
+            $('a[rel="ID_STAGE_<?=$id_v ?>"]').editable({'disabled':false,'showbuttons':false,'name':'ID_STAGE','title':'Выберите платформу','value':null,'url':'<?= Yii::app()->createUrl('Autorized/updateProfile') ?>','type':'select','placement':'top','source':[
+                {'value':'1','text':'Онкология'},
+                {'value':'2','text':'Кардиология и ангиология'},
+                {'value':'3','text':'Неврология'},
+                {'value':'4','text':'Эндокринология'},
+                {'value':'5','text':'Педиатрия'},
+                {'value':'6','text':'Психиатрия и зависимости'},
+                {'value':'7','text':'Имунология'},
+                {'value':'8','text':'Микробиология'},
+                {'value':'9','text':'Фaрмакология'},
+                {'value':'10','text':'Профилактическая среда'},
+                {'value':'11','text':'Репродуктивное здоровье'},
+                {'value':'12','text':'Регенеративная медицина'},
+                {'value':'13','text':'Инвазивные технологии'},
+                {'value':'14','text':'Инновационные фундаментальные технологии в медицине'},
+
+
+            ]});
+
+            <? endforeach; ?>
         });
 
     });
