5e2a772403a0cb8a7efcb56e320db8157f70d27d
Altenrion <landerfeld@gmail.com>
2014-11-04 22:54:19 +0400

    добавил факт ролей финансовых ... да да, этих самы�

--- protected/controllers/AutorizedController.php
+++ protected/controllers/AutorizedController.php
@@ -1198,14 +1198,25 @@ class AutorizedController extends Controller
 
             /** Критерий для эксперта 2 уровня (по платформе и округу)*/
             case 'Exp2' :
-                $criteria->condition = 't.ID_STAGE = :stage AND us.ID_DISTRICT = :dist AND FIRST_LAVEL_APPROVAL = 3';
-                $criteria->params = array(":stage" => $user['ID_STAGE'], ":dist" => $user['ID_DISTRICT']);
+
+                if($this->checkFinanceBustersRole()){
+
+                }
+                else{
+                    $criteria->condition = 't.ID_STAGE = :stage AND us.ID_DISTRICT = :dist AND FIRST_LAVEL_APPROVAL = 3';
+                    $criteria->params = array(":stage" => $user['ID_STAGE'], ":dist" => $user['ID_DISTRICT']);
+                }
                 break;
 
             /** Критерий для эксперта 3 уровня (по платформе) */
             case 'Exp3' :
+                if($this->checkFinanceBustersRole()){
+
+                }
+                else{
                 $criteria->condition = 't.ID_STAGE = :stage SECOND_LEVEL_RATING NOT NULL';
                 $criteria->params = array(":stage" => $user['ID_STAGE']);
+                }
                 break;
 
             /** Критерий для разработчика */
@@ -1215,6 +1226,11 @@ class AutorizedController extends Controller
 
         return $criteria;
     }
+
+    public function checkFinanceBustersRole(){
+        return false;
+    }
+
 
     public function actionSaveProjectComment(){
         if(isset($_POST['comment'])){
