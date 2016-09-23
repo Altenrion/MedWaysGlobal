17a88d25ad33b7c122d1970a57f4bbda0cbaa80b
Nikita Baryshev <landerfeld@gmail.com>
2016-09-23 16:08:28 +0300

    ! changed development settings

--- index.php
+++ index.php
@@ -1,38 +1,43 @@
 <?php
 
-        /**
-         * Определение среды
-        */
 
-        defined('APPLICATION_ENV')
-        || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-        // Подключаем Yii
-        $yii=dirname(__FILE__).'/framework/yii.php';
+/** Указание среды */
 
+if (!defined('APPLICATION_ENV')) {
+    if ($_SERVER['SERVER_NAME'] == "medwaysglobal") {
+        define(APPLICATION_ENV, 'development');
+    } else {
+        define(APPLICATION_ENV, 'production');
+    }
+}
 
-        /**
-         * Включим дебаг если мы разработчики
-         */
-        if (APPLICATION_ENV == 'devel')
-        {
-            defined('YII_DEBUG') or define('YII_DEBUG',true);
-            defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
-        }
+/** Определение среды */
 
-        /**
-         * А вот так мы подключим нужную нам конфигурацию
-         */
-        $config = dirname(__FILE__).'/protected/config/'.APPLICATION_ENV.'.php';
-
-        require_once($yii);
-
-        $YiiApp = Yii::createWebApplication($config);
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+// Подключаем Yii
+$yii = dirname(__FILE__) . '/framework/yii.php';
 
 
-        /**
-         * Тут можно подключить много всяких штук, но каунтер браузеров
-         *  перенесен в beforeAction класса Controller
-         */
+/** Включим дебаг если мы разработчики */
 
-        $YiiApp->run();
+if (APPLICATION_ENV == 'development') {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+}
+
+/** Подключим нужную нам конфигурацию */
+
+$config = dirname(__FILE__) . '/protected/config/' . APPLICATION_ENV . '.php';
+
+
+
+require_once($yii);
+$YiiApp = Yii::createWebApplication($config);
+
+/**
+ * Тут можно подключить много всяких штук, но каунтер браузеров
+ *  перенесен в beforeAction класса Controller
+ */
+
+$YiiApp->run();
 
