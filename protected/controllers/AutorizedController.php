7a16806968807841d2f32bba6dded550fdfa448f
Nikita Baryshev <landerfeld@gmail.com>
2016-11-14 23:04:52 +0300

    ! ff

--- protected/controllers/AutorizedController.php
+++ protected/controllers/AutorizedController.php
@@ -1247,7 +1247,7 @@ class AutorizedController extends Controller
         $criteria->select = "t.ID_UNIVER ,t.ID_DISTRICT ,
         (SELECT COUNT(DISTINCT id) FROM `m_w_users` WHERE roles IN ('Exp','Exp1','Exp2','Exp3') AND ID_UNIVER = t.ID_UNIVER ) as ExpCount,
         (SELECT COUNT(DISTINCT id) FROM m_w_users as u  Where roles IN ('Manager') AND ID_UNIVER = t.ID_UNIVER AND u.REG_DATE > '2016-09-01' ) as ProjCount,
-        (SELECT COUNT(DISTINCT id) FROM m_w_users as u  Where roles IN ('Moder, Moder1') AND ID_UNIVER = t.ID_UNIVER ) as UniverModer";
+        (SELECT COUNT(DISTINCT id) FROM m_w_users as u  Where roles IN ('Moder', 'Moder1') AND ID_UNIVER = t.ID_UNIVER ) as UniverModer";
         $criteria->condition = "AKTIV_KEY='100' AND ID_UNIVER is not NULL AND ID_DISTRICT is not NULL AND t.REG_DATE > '2016-09-01'";
         $criteria->group = 'ID_UNIVER';
 
